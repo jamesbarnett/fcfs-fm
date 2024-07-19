3db9a22a87bcdffbbd73b65ead2cda2b24dd6aa0
Jim Barnett <james.barnett.jr@gmail.com>
2024-07-19 05:42:01 -0500

    Fine-tuned font stuff

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,14 @@
   import Card from './Card.svelte';
 </script>
 <main class="bg-neutral-lightgray px-8 py-20">
-  <section class="pitch text-center pb-20">
-    <h1 class="text-2xl font-light leading-loose sm:text-4xl">Reliable, efficient delivery</h1>
-    <p class="text-2xl font-bold pb-4 sm:text-4xl">
+  <article class="pitch text-center pb-20">
+    <h1 class="text-2xl font-light leading-loose sm:text-4xl sm:tracking-[.01em]">Reliable, efficient delivery</h1>
+    <p class="text-2xl font-bold pb-4 sm:text-4xl sm:leading-relaxed sm:tracking-[.01em]">
       Powered by Technology
     </p>
     <p class="pitch-details font-light leading-relaxed text-center">Our Artificial Intelligence powered tools use millions of project data points 
       to ensure that your project is successful</p>
-  </section>
+  </article>
   <section class="flex flex-col gap-8 sm:grid sm:grid-cols-3 sm:items-center sm:max-w-[72rem] sm:mx-auto sm:gap-4">
     <Card styles="sm:row-start-1 sm:row-span-2"
       name="Supervisor"
